f9647d434a2acaf09752ad642211b0fff8a35b7b
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-09-23 11:44:13 -0400

    fix: updated Handler interface

--- interfaces.go
+++ interfaces.go
@@ -8,7 +8,7 @@ type (
 	// Handler is the interface to handle the Hailo CLIP application
 	Handler interface {
 		GenerateEmbeddings(ctx context.Context) error
-		Run(ctx context.Context, stopFn func()) error
+		Run(ctx context.Context, cancelFn context.CancelFunc) error
 		IsRunning() bool
 		GetClassification() (*Classification, error)
 	}
